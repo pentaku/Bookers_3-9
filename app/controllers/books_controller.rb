5ef08b35fd31161e9bc613ae6dc33d12dd8d2bdb
pentaku <ohara1728136@gmail.com>
2021-08-22 05:51:41 +0000

    [update]Editページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,8 +13,23 @@ class BooksController < ApplicationController
   def create
     booklist = Booklist.new(booklist_params)
     booklist.save
-    redirect_to '/'
+    redirect_to book_path(booklist.id)
   end
+
+  def show
+     @booklist = Booklist.find(params[:id])
+  end
+
+  def edit
+    @booklist = Booklist.find(params[:id])
+  end
+
+  def update
+    booklist = Booklist.find(params[:id])
+    booklist.update(booklist_params)
+    redirect_to book_path(booklist.id)
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/' => 'homes#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
